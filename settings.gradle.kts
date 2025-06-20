d312e8596693661d34aa809b8ee3eca66e8346e2
lyc <3090487933@qq.com>
2025-06-20 20:04:52 +0800

    模块化项目

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,20 @@ dependencyResolutionManagement {
 
 rootProject.name = "NewsApp"
 include(":app")
- 
+include(":feature")
+include(":core")
+include(":core:common")
+include(":core:data")
+include(":core:database")
+include(":core:designsystem")
+include(":core:domain")
+include(":core:model")
+include(":core:network")
+include(":core:ui")
+include(":feature:home")
+include(":feature:search")
+include(":feature:profile")
+include(":feature:interests")
+include(":feature:newsdetail")
+include(":feature:auth")
+include(":feature:bookmarks")
